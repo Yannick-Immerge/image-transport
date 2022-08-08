33f8c8c7ccc56132960a8fa0170649e74d12e45e
Yannick-Immerge <immerge.yannick@gmail.com>
2022-08-08 17:37:53 +0200

    Implementing windows application in c#

--- scripts/windows/main.ps1
+++ scripts/windows/main.ps1
@@ -4,7 +4,7 @@
 
 .DESCRIPTION
     The Image Transportation Tool is a tool that uses a self-contained docker environment 
-    to create a local repository bound to local storage to transport images from one context
+    to create a local repository bound to local storage. This repo is then used to transport images from one context
     to another without relying on any external services. The tool should be executed on one machine
     that has access to the target image in write mode. When the image is transferred to the local volume 
     it can be mounted to the second machine. There the tool should be executed in read mode to move the image 
@@ -13,33 +13,38 @@
 .PARAMETER Mode
     READ or WRITE depending on the mode the tool should be executed in.
 
-.EXAMPLE
-     Invoke-Image
+.PARAMETER ImageName
+    The full name of an image (including label) with the repository prefix that lives outside of the local repo.
+
+.PARAMETER LocalName
+    The full name of an image (including label) without the repository prefix that lives inside of the local repo.
 
 .EXAMPLE
-     'Server1', 'Server2' | Get-MrAutoStoppedService
+     Invoke-ImageTransportTool Write -ImageName myrepo/myimage:1.0 -LocalName myportableimage:1.0:
 
 .EXAMPLE
-     Get-MrAutoStoppedService -ComputerName 'Server1' -Credential (Get-Credential)
+    imagett read myportableimage:1.0 otherrepo/myimage:1.0
 
 .INPUTS
     String
 
-.OUTPUTS
-    PSCustomObject
-
 .NOTES
-    Author:  Mike F Robbins
-    Website: http://mikefrobbins.com
-    Twitter: @mikefrobbins
+    Author:  Yannick Bergs
+    Github: http://github.com/Yannick-Immerge
 #>
 function Invoke-ImageTransportTool{
+    
     [CmdletBinding()]
-    function(
-        [Parameter]
-        [switch]$Help
-        [Parameter]
-        [string]$Mode
+    [Alias("imagett")]
+    param(
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [string]$Mode,
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [string]$LocalName,
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [string]$ImageName,
+        [Parameter(Mandatory=$false)]
+        [string]$BuildPath
     )
 
     Write-Output "Thanks for using the Docker Image Transport Tool."
@@ -48,11 +53,6 @@ function Invoke-ImageTransportTool{
     Import-Module "./assure_install.ps1"
     Install-DockerVersion
 
-    if(-not $PSBoundParameters.ContainsKey("Mode")){
-        Write-Output "Please specify now how you intend to use the tool. `nChoose between [R]ead or [W]rite."
-        $Mode = (Read-Host -Prompt "Run Mode")
-    }
-    
 }
 function Invoke-ImageWrite{
    
